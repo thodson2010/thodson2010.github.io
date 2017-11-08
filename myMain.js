fdc8c555698819c748f3b73404c1bba6d5321807
Sammortinger1 <33103749+Sammortinger1@users.noreply.github.com>
2017-11-08 12:22:47 -0500

    Updates
    Fixed styling on description
    Added ids to each filter option in index.html, then wrote a rough outline for filtering process in myMain.js
    also cleaned up reference calls for crime type in color setting in myMain.js

--- myMain.js
+++ myMain.js
@@ -4,38 +4,34 @@ var myFunctionHolder = {};
 //declaring function 1
 myFunctionHolder.addPopups = function (feature, layer) {
   if (feature.properties && feature.properties.Location) {
-    var list = "<dl><dt>Incident Type:</dt>"
-    + "<dd>" + feature.properties["Incident Type"] + "</dd>"
-    + "<dt>Description:</dt>"
-    + "<dd>" + feature.properties["Description"] + "</dd>"
-    layer.bindPopup(list);
+    var incLocation = feature.properties.Location
+    var endLoc = incLocation.indexOf("Ohio");
+    incLocation = incLocation.substring(0, endLoc-1);
+    layer.bindPopup("<b>Crime Type : </b>" + feature.properties["Incident Type"] + "<br><b> Location : </b>" + incLocation);
   }
-}
-
-myFunctionHolder.addDescription = function (feature) {
-  document.getElementById("description-box").innerHTML = "<p><b>Description : </b> + feature.properties.Description</p>";
 }
 
 //declaring function 2
 myFunctionHolder.pointToCircle = function (feature, latlng) {
   var fillColorVar = "white";
-  if(feature.properties["Incident Type"].includes("Crash"))
+  var type = feature.properties["Incident Type"];
+  if(type.includes("Crash"))
   {
     fillColorVar = "blue";
   }
-  else if(feature.properties["Incident Type"].includes("Drug"))
+  else if(type.includes("Drug"))
+  {
+    fillColorVar = "purple";
+  }
+  else if(type.includes("Theft") || type.includes("Burglary") || type.includes("Breaking"))
   {
     fillColorVar = "green";
   }
-  else if(feature.properties["Incident Type"].includes("Theft") || feature.properties["Incident Type"].includes("Burglary") || feature.properties["Incident Type"].includes("Breaking"))
-  {
-    fillColorVar = "orange";
-  }
-  else if(feature.properties["Incident Type"].includes("Assault") || feature.properties["Incident Type"].includes("Criminal"))
+  else if(type.includes("Assault") || type.includes("Criminal"))
   {
     fillColorVar = "red";
   }
-  else if(feature.properties["Incident Type"].includes("Administrative") || feature.properties["Incident Type"].includes("Assist"))
+  else if(type.includes("Administrative") || type.includes("Assist"))
   {
     fillColorVar = "yellow";
   }
@@ -49,10 +45,15 @@ myFunctionHolder.pointToCircle = function (feature, latlng) {
   };
   var circleMarker = L.circleMarker(latlng, geojsonMarkerOptions);
   circleMarker.on('click', function(){
-        var x = "<p><b>Description : </b>" + feature.properties.Description + "</p>";
-        document.getElementById("description-box").innerHTML = x;
+        var description = "<p><b>Description : </b>" + feature.properties.Description + "</p>";
+        document.getElementById("description-box").innerHTML = description;
     });
   return circleMarker;
+}
+
+//this would be where we filter data, for now just prints the type to console
+myFunctionHolder.filter = function(type){
+	console.log(type);
 }
 
 //execute
@@ -68,6 +69,11 @@ window.onload = function () {
     onEachFeature: myFunctionHolder.addPopups,
     pointToLayer: myFunctionHolder.pointToCircle
   });
+  
+  //add event listeners
+  var filter1 = document.getElementById("filter1").addEventListener('click', myFunctionHolder.filter(1));
+  var filter2 = document.getElementById("filter2").addEventListener('click', myFunctionHolder.filter(2));
+  var filter3 = document.getElementById("filter3").addEventListener('click', myFunctionHolder.filter(3));
 
   mapObject.addLayer(crimeLayerGroup);
   mapObject.fitBounds(crimeLayerGroup.getBounds());
