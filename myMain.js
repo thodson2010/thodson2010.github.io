7e535878074eef73ee89e22a69e13e49cfb259b7
Sammortinger1 <33103749+Sammortinger1@users.noreply.github.com>
2017-11-13 22:23:41 -0500

    Updated with Description Box and Filtering
    Filtering works, but is not the most flexible/optimized possible. I will work to streamline the process later.

--- myMain.js
+++ myMain.js
@@ -7,9 +7,56 @@ myFunctionHolder.addPopups = function (feature, layer) {
     var incLocation = feature.properties.Location
     var endLoc = incLocation.indexOf("Ohio");
     incLocation = incLocation.substring(0, endLoc-1);
-    layer.bindPopup("<b>Crime Type : </b>" + feature.properties["Incident Type"] + "<br><b> Location : </b>" + incLocation);
+    //layer.bindPopup("<b>Crime Type : </b>" + feature.properties["Incident Type"] + "<br><b> Location : </b>" + incLocation);
   }
 }
+
+//Filter for each crime type: Need to find way of condensing to one method
+myFunctionHolder.filterTheft = function(feature){
+  var type = feature.properties["Incident Type"];
+  if (type.includes("Theft") || type.includes("Burglary") || type.includes("Breaking"))
+  {
+    return true;
+  }
+}
+myFunctionHolder.filterCrash = function(feature){
+  var type = feature.properties["Incident Type"];
+  if (type.includes("Crash"))
+  {
+    return true;
+  }
+}
+myFunctionHolder.filterDrug = function(feature){
+  var type = feature.properties["Incident Type"];
+  if (type.includes("Drug"))
+  {
+    return true;
+  }
+}
+myFunctionHolder.filterAdmin = function(feature){
+  var type = feature.properties["Incident Type"];
+  if (type.includes("Administrative") || type.includes("Assist"))
+  {
+    return true;
+  }
+}
+myFunctionHolder.filterAssault = function(feature){
+  var type = feature.properties["Incident Type"];
+  if (type.includes("Assault") || type.includes("Criminal"))
+  {
+    return true;
+  }
+}
+myFunctionHolder.filterOther = function(feature){
+  var type = feature.properties["Incident Type"];
+  if (!type.includes("Assault") && !type.includes("Criminal") && !type.includes("Administrative") && !type.includes("Assist") && !type.includes("Drug") && !type.includes("Crash") && !type.includes("Theft")
+  && !type.includes("Burglary") && !type.includes("Breaking"))
+  {
+    return true;
+  }
+}
+
+
 
 //declaring function 2
 myFunctionHolder.pointToCircle = function (feature, latlng) {
@@ -45,15 +92,20 @@ myFunctionHolder.pointToCircle = function (feature, latlng) {
   };
   var circleMarker = L.circleMarker(latlng, geojsonMarkerOptions);
   circleMarker.on('click', function(){
+        geojsonMarkerOptions.color = "#FFF"; //not working, need to fix
+        //writing description
         var description = "<p><b>Description : </b>" + feature.properties.Description + "</p>";
-        document.getElementById("description-box").innerHTML = description;
+        //writing location, trim off end
+        var incLocation = feature.properties.Location
+        var endLoc = incLocation.indexOf("Ohio");
+        incLocation = incLocation.substring(0, endLoc-1);
+        var location = "<p><b>Location: </b>" + incLocation + "</p>";
+        //writing type
+        var type = "<p><b>Crime Type : </b>" + feature.properties["Incident Type"] + "</p>";
+        //combining
+        document.getElementById("description-box").innerHTML = description + location + type;
     });
   return circleMarker;
-}
-
-//this would be where we filter data, for now just prints the type to console
-myFunctionHolder.filter = function(type){
-	console.log(type);
 }
 
 //execute
@@ -64,17 +116,94 @@ window.onload = function () {
     attribution: "&copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
   }).addTo(mapObject);
 
-  // bikeThefts is the variable name we difined in Bike_Thefts_2011.js file.
-  var crimeLayerGroup = L.geoJSON(crimeData, {
-    onEachFeature: myFunctionHolder.addPopups,
+  //Create each layer group
+  var theftLayerGroup = L.geoJSON(crimeData, {
+    filter: myFunctionHolder.filterTheft,
     pointToLayer: myFunctionHolder.pointToCircle
   });
-  
-  //add event listeners
-  var filter1 = document.getElementById("filter1").addEventListener('click', myFunctionHolder.filter(1));
-  var filter2 = document.getElementById("filter2").addEventListener('click', myFunctionHolder.filter(2));
-  var filter3 = document.getElementById("filter3").addEventListener('click', myFunctionHolder.filter(3));
+  var crashLayerGroup = L.geoJSON(crimeData, {
+    filter: myFunctionHolder.filterCrash,
+    pointToLayer: myFunctionHolder.pointToCircle
+  });
+  var drugLayerGroup = L.geoJSON(crimeData, {
+    filter: myFunctionHolder.filterDrug,
+    pointToLayer: myFunctionHolder.pointToCircle
+  });
+  var adminLayerGroup = L.geoJSON(crimeData, {
+    filter: myFunctionHolder.filterAdmin,
+    pointToLayer: myFunctionHolder.pointToCircle
+  });
+  var assaultLayerGroup = L.geoJSON(crimeData, {
+    filter: myFunctionHolder.filterAssault,
+    pointToLayer: myFunctionHolder.pointToCircle
+  });
+  var otherLayerGroup = L.geoJSON(crimeData, {
+    filter: myFunctionHolder.filterOther,
+    pointToLayer: myFunctionHolder.pointToCircle
+  });
 
-  mapObject.addLayer(crimeLayerGroup);
-  mapObject.fitBounds(crimeLayerGroup.getBounds());
+  //originally show every layer
+  mapObject.addLayer(theftLayerGroup);
+  mapObject.addLayer(crashLayerGroup);
+  mapObject.addLayer(drugLayerGroup);
+  mapObject.addLayer(adminLayerGroup);
+  mapObject.addLayer(assaultLayerGroup);
+  mapObject.addLayer(otherLayerGroup);
+
+  //add overlay for toggling crime types, id = "toggles"
+  var legend = L.control({position: 'bottomleft'});
+
+  //Adds toggles to map
+  legend.onAdd = function (map) {
+      var div = L.DomUtil.create('div', 'toggles');
+      div.innerHTML = "<b>Filters: </b><form><input type = \"checkbox\" id=\"theftToggle\" checked><label>Theft</label></input><input type = \"checkbox\" id = \"crashToggle\" checked><label>Traffic Incident</label></input></form><input type = \"checkbox\" id=\"drugToggle\" checked><label>Drug-related</label></input><input type = \"checkbox\" id=\"adminToggle\" checked><label>Administrative</label></input><input type = \"checkbox\" id=\"assaultToggle\" checked><label>Criminal Incident</label></input><input type = \"checkbox\" id=\"otherToggle\" checked><label>Other</label></input>";
+      return div;
+  };
+
+  var overlays = {
+    "Thefts": theftLayerGroup
+  };
+  mapObject.fitBounds(adminLayerGroup.getBounds());
+  legend.addTo(mapObject);
+
+  //Checks for each toggle
+  $('#theftToggle').change(function() {
+      if (this.checked)
+        mapObject.addLayer(theftLayerGroup);
+      else
+        mapObject.removeLayer(theftLayerGroup);
+  });
+
+  $('#crashToggle').change(function() {
+      if (this.checked)
+        mapObject.addLayer(crashLayerGroup);
+      else
+        mapObject.removeLayer(crashLayerGroup);
+  });
+
+  $('#drugToggle').change(function() {
+      if (this.checked)
+        mapObject.addLayer(drugLayerGroup);
+      else
+        mapObject.removeLayer(drugLayerGroup);
+  });
+
+  $('#adminToggle').change(function() {
+      if (this.checked)
+        mapObject.addLayer(adminLayerGroup);
+      else
+        mapObject.removeLayer(adminLayerGroup);
+  });
+  $('#assaultToggle').change(function() {
+      if (this.checked)
+        mapObject.addLayer(assaultLayerGroup);
+      else
+        mapObject.removeLayer(assaultLayerGroup);
+  });
+  $('#otherToggle').change(function() {
+      if (this.checked)
+        mapObject.addLayer(otherLayerGroup);
+      else
+        mapObject.removeLayer(otherLayerGroup);
+  });
 };
